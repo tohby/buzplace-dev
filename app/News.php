afa8ce8c0a7fe90f6deded1ebc0f154f73360ec9
tadeyinka <tohbyyy@gmail.com>
2020-07-14 17:55:05 +0700

    Update laravel Core and install Canvas

--- app/News.php
+++ app/News.php
@@ -2,37 +2,17 @@
 
 namespace App;
 
-use Cviebrock\EloquentSluggable\Sluggable;
-use Cviebrock\EloquentSluggable\SluggableScopeHelpers;
 use Illuminate\Database\Eloquent\Model;
 
 class News extends Model
 {
-    //
-
-    use Sluggable;
-    use SluggableScopeHelpers;
 
     protected $fillable = [
-        'headline', 'image', 'content',
+        'headline', 'image', 'content', 'slug'
     ];
-
-    public function sluggable(){
-        return [
-            'slug' => [
-                'source' => 'headline'
-            ]
-        ];
-    }
 
     public function comment() {
         return $this->hasMany('App\Comment');
     }
 
-    protected $shareOptions = [
-        'columns' => [
-            'title' => 'headline'
-        ],
-        'url' => null
-    ];
 }
