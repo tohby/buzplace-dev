85fb5a8ab40324ed66bbfc067ac2ebc23c20bebd
tadeyinka <tohbyyy@gmail.com>
2020-07-16 14:36:18 +0700

    Canvas setup
    Canvas front end setup

---
+++ app/Canvas.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Canvas extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'canvas_posts';
+}
