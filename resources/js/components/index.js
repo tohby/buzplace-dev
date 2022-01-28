c57bbf8d55093e058bafdc6f898b662a348a882f
Tobi <tohbyyy@gmail.com>
2022-01-28 23:04:48 +0700

    Upgrade Laravel version

--- resources/js/components/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Messages from './messages/Messages';
-import './messages/Messages.css';
-
-if (document.getElementById('messages')) {
-    ReactDOM.render(<Messages />, document.getElementById('messages'));
-}
