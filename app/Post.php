f263fadbcb7be18eda76cd6f7e87c5f2c61897a3
Tobi <tohbyyy@gmail.com>
2022-01-29 15:29:58 +0700

    remove unused packages

--- app/Post.php
+++ app/Post.php
@@ -2,26 +2,12 @@
 
 namespace App;
 
-use Cviebrock\EloquentSluggable\Sluggable;
-use Cviebrock\EloquentSluggable\SluggableScopeHelpers;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
-
-    use Sluggable;
-    use SluggableScopeHelpers;
 
     protected $fillable = ['user_id', 'image', 'title', 'content'];
-
-    public function sluggable() {
-        return [
-            'slug' => [
-                'source' => 'title'
-            ]
-        ];
-    }
 
     public function user() {
         return $this->belongsTo('App\User');
